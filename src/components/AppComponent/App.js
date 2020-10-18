23d8e09191d10e94cebda02f8cfc697e9750d460
Emmanuel Maro <emmanuelmaro733@gmail.com>
2020-10-19 00:05:08 +0300

    add images to readme file

--- src/components/AppComponent/App.js
+++ src/components/AppComponent/App.js
@@ -6,8 +6,13 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1> Hello, World! </h1>
-                <p>Congratulation, You May now can start using React!</p>
+                <div className="App-header">
+                    <h2>Welcome to React</h2>
+                </div>
+
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
             </div>
         );
     }
